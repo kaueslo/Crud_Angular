2ab8d9477cb0ea9dbdc1d987283e9c6b84768352
Kaue Sloboda <kaue.sloboda@hotmail.com>
2026-01-06 23:15:23 -0300

    iniciando a tela de consulta de cadastro

--- src/app/clienteService.ts
+++ src/app/clienteService.ts
@@ -5,11 +5,35 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ClienteService {
+
+  static REPO_CLIENTES = "_CLIENTES";
   
   constructor(){}
 
   salvar(cliente: Cliente){
+    const storage = this.obterStorage();
 
+    storage.push(cliente);
+
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage));
+  }
+
+  pesquisarClientes(nome: string) : Cliente[]{
+    return this.obterStorage();
+  }
+
+  //private pois só essa Service vai acessar o obterStorage
+  private obterStorage() : Cliente[]{
+    const repositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
+
+    if(repositorioClientes){
+      const clientes: Cliente[] = JSON.parse(repositorioClientes);
+      return clientes;
+    }
+
+    const clientes: Cliente[] = [];
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes));
+    return clientes;
   }
 
 }
--- src/app/consulta/consulta.ts
+++ src/app/consulta/consulta.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatAnchor } from "@angular/material/button";
+import { ClienteService } from '../clienteService';
+import { Cliente } from '../cadastro/cliente';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatIconModule,
+    FormsModule,
+    MatTableModule,
+    MatAnchor
+],
   templateUrl: './consulta.html',
   styleUrl: './consulta.scss',
 })
-export class Consulta {
+export class Consulta implements OnInit {
+
+  listaClientes: Cliente[] = [];
+
+  constructor(private service: ClienteService){
+
+  }
+
+  ngOnInit(){
+    this.listaClientes = this.service.pesquisarClientes('');
+  }
 
 }
